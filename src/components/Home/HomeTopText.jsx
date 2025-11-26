c7a811172f93e66a5a1a9175ce3364a032fdb2c9
nidhi <kumarinidhi0594@gmail.com>
2025-11-26 12:50:48 +0100

    design3

--- src/components/Home/HomeTopText.jsx
+++ src/components/Home/HomeTopText.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Video from './Video'
 
 const HomeTopText = () => {
     return (
-        <div className='font-[font1] text-white'>
-            <div className='text-[12vw] uppercase leading-[10vw]' > L'étincelle </div>
-            <div className='text-[12vw] uppercase leading-[10vw]'> quigénère </div>
-            <div className='text-[12vw] uppercase leading-[10vw]'> la créativité </div>
+        <div className='font-[font1] text-white pt-10 text-center'>
+            <div className='text-[9.5vw] uppercase leading-[9.5vw]' > L'étincelle </div>
+            <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center'>
+                 qui
+                <div className='h-[10vw] w-[10vw]'> <Video /> </div>
+                génère </div>
+            <div className='text-[9.5vw] uppercase leading-[9.5vw]'> la créativité </div>
 
         </div>
     )
