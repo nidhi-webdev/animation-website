30fd6a6b58b54a86e2521468d41c516ba606b5f4
nidhi <kumarinidhi0594@gmail.com>
2025-11-26 11:11:05 +0100

    Router Added and Navigating in the other page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div>
+    <div className='text-white'>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={<Projects />}  />
       </Routes>
       
     </div>
