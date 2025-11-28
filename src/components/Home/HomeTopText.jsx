5edab3de36f27cb699dff2a1f11722c12f0176a6
nidhi <kumarinidhi0594@gmail.com>
2025-11-28 23:40:37 +0100

    design5

--- src/components/Home/HomeTopText.jsx
+++ src/components/Home/HomeTopText.jsx
@@ -7,7 +7,7 @@ const HomeTopText = () => {
             <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center' > L'étincelle </div>
             <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center'>
                  qui
-                <div className='h-[10vw]'> <Video /> </div>
+                <div className='h-[2vw] w-[2vw]'> <Video /> </div>
                 génère </div>
             <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center'> la créativité </div>
 
