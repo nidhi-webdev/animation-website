c6e75415f817fd6a9e9f0db286e7eb5130b98493
nidhi <kumarinidhi0594@gmail.com>
2025-11-28 23:43:42 +0100

    design7

--- src/components/Home/HomeTopText.jsx
+++ src/components/Home/HomeTopText.jsx
@@ -4,10 +4,10 @@ import Video from './Video'
 const HomeTopText = () => {
     return (
         <div className='font-[font2] text-white pt-5 text-center '>
-            <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center' > L'étincelle </div>
-            <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center'>
+            <div className='text-[9.5vw] uppercase leading-[8vw] flex items-center justify-center' > L'étincelle </div>
+            <div className='text-[9.5vw] uppercase leading-[8vw] flex items-center justify-center'>
                  qui
-                <div className='h-[1vw] w-3xs'> <Video /> </div>
+                <div className='h-[1vw]'> <Video /> </div>
                 génère </div>
             <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center'> la créativité </div>
 
