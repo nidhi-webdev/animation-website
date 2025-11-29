aeac0ad546343e3e076022819a5eb7622d0ff367
nidhi <kumarinidhi0594@gmail.com>
2025-11-29 17:08:40 +0100

    Bottom text design

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='font-[font1] text-white'>
+    <div className='font-[font1] text-white flex items-center justify-center gap-2'>
       <Link> Projects  </Link>
       <Link> Agence  </Link>
     </div>
