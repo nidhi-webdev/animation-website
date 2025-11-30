523d787e11bd202ae2d887704c82a6844424699d
nidhi <kumarinidhi0594@gmail.com>
2025-11-30 19:02:30 +0100

    design done

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className="h-[8vw] flex justify-center items-center gap-8">
-      <Link className="text-[6.5vw] font-bold uppercase border-4 border-white px-[2vw] py-[0.2vw] leading-none flex items-center justify-center text-white bg-transparent rounded-[999px] box-border">
+      <Link className="text-[6.5vw] font-bold uppercase border-3 border-white px-[2vw] py-[0.2vw] leading-none flex items-center justify-center text-white bg-transparent rounded-[999px] box-border">
         PROJETS
       </Link>
-      <Link className="text-[6.5vw] font-bold uppercase border-4 border-white px-[2vw] py-[0.2vw] leading-none flex items-center justify-center text-white bg-transparent rounded-[999px] box-border">
+      <Link className="text-[6.5vw] font-bold uppercase border-3 border-white px-[2vw] py-[0.2vw] leading-none flex items-center justify-center text-white bg-transparent rounded-[999px] box-border">
         AGENCE
       </Link>
     </div>
