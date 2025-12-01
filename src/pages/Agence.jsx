6d093c244a9e5a20735015391287609db3dfd9a2
nidhi <kumarinidhi0594@gmail.com>
2025-12-01 19:02:29 +0100

    Agence design text

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -7,6 +7,15 @@ const Agence = () => {
         <h1 className='text-white text-[20vw] uppercase text-center leading-[18vw]'>Soixan7e <br />
           Douze</h1>
       </div>
+      <div className='pl-[50%] bg-red-500'>
+        <p className='text-5xl text-white'>
+          Notre curiosité nourrit notre créativité.
+          On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante.
+          Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de
+          bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage
+          à donner de la perspective, pour bâtir des marques influentes.
+        </p>
+      </div>
     </div>
   )
 }
