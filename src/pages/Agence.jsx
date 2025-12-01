4a1ad4cbe693f3e9fb53bbe595b7f6a41c5d7bba
nidhi <kumarinidhi0594@gmail.com>
2025-12-01 18:58:16 +0100

    Agence design

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Agence = () => {
   return (
     <div className='font-[font1]'>
-      <div className='bg-red-600 mt-[55vh]'>
+      <div className='mt-[55vh]'>
         <h1 className='text-white text-[20vw] uppercase text-center leading-[18vw]'>Soixan7e <br />
           Douze</h1>
       </div>
