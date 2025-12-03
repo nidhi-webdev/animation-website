c450bdcf3cccd9566233974c3c4a36dc21adcb58
nidhi <kumarinidhi0594@gmail.com>
2025-12-03 16:26:45 +0100

    Agence Page design

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,26 +2,31 @@ import React from 'react'
 
 const Agence = () => {
   return (
-   <div>
-    <div className='h-[20vw] w-[15vw] absolute top-60 left-[30vw] rounded-3xl'>
-      <img src='https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg' />
-    </div>
-     <div className='font-[font1] relative'>
-      <div className='mt-[55vh]'>
-        <h1 className='text-white text-[20vw] uppercase text-center leading-[18vw]'>Soixan7e <br />
-          Douze</h1>
+    <div>
+      <div className='section1'>
+        <div className='h-[20vw] w-[15vw] absolute top-60 left-[30vw] rounded-4xl overflow-hidden'>
+          <img className='h-full w-full object-cover' src='https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg' />
+        </div>
+        <div className='font-[font1] relative'>
+          <div className='mt-[55vh]'>
+            <h1 className='text-white text-[20vw] uppercase text-center leading-[18vw]'>Soixan7e <br />
+              Douze</h1>
+          </div>
+          <div className='pl-[40%] mt-20'>
+            <p className='text-5xl text-white'>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité.
+              On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante.
+              Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de
+              bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage
+              à donner de la perspective, pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className='pl-[40%] mt-20'>
-        <p className='text-5xl text-white'>
-         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité.
-          On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante.
-          Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de
-          bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage
-          à donner de la perspective, pour bâtir des marques influentes.
-        </p>
+      <div className="section2 h-screen">
+
       </div>
     </div>
-   </div>
   )
 }
 
