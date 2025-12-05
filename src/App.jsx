a9ef9ed3ab83877725e6fca11a56a47c06d4b486
nidhi <kumarinidhi0594@gmail.com>
2025-12-05 18:53:37 +0100

    Animation that black and white but not working

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,44 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from "gsap"
+
+
+
 
 const App = () => {
+
+
+  useGSAP(function () {
+    gsap.from('.stair', {
+      height: 0,
+      duration: 2,
+      stagger: {
+        amount: -0.3
+      }
+    })
+  })
+
+
+
   return (
     <div className='text-white'>
-      <div className='h-screen w-full flex fixed z-20 top-0'>
-        <div className='h-full w-1/5 bg-black'> </div>
-        <div className='h-full w-1/5 bg-white'> </div>
-        <div className='h-full w-1/5 bg-black'> </div>
-        <div className='h-full w-1/5 bg-white'> </div>
-        <div className='h-full w-1/5 bg-black'> </div>
+      <div className='stair h-screen w-full flex fixed z-20 top-0'>
+        <div className='stair h-full w-1/5 bg-black'> </div>
+        <div className='stair h-full w-1/5 bg-black'> </div>
+        <div className='stair h-full w-1/5 bg-black'> </div>
+        <div className='stair h-full w-1/5 bg-black'> </div>
+        <div className='stair h-full w-1/5 bg-black'> </div>
 
       </div>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
-        <Route path='/projects' element={<Projects />}  />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
-      
+
     </div>
   )
 }
