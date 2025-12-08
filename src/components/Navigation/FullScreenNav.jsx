6a38ac3bf4e17af1e6021ff426e09f75d2dd273c
nidhi <kumarinidhi0594@gmail.com>
2025-12-08 20:40:01 +0100

    Image for navbar

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 const FullScreenNav = () => {
-  return (
-    <div>
-      <div>
+    return (
         <div>
-            <h1> Projects</h1>
-            <div><h2>Pour tout voir </h2> </div>
+            <div>
+                <div>
+                    <h1> Projects</h1>
+                    <div><h2>Pour tout voir </h2>
+                        <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                    </div>
+
+                    <div><h2>Pour tout voir </h2>
+                        <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default FullScreenNav
