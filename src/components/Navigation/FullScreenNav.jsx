5c2512ddee5053f8553d34fadbc8621155e9fddc
nidhi <kumarinidhi0594@gmail.com>
2025-12-08 20:35:04 +0100

    FullScreen div

---
+++ src/components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1> Projects</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
