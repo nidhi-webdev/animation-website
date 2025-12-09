f46358edae8aac6519b4f71923821ccb68bfbce1
nidhi <kumarinidhi0594@gmail.com>
2025-12-09 18:40:03 +0100

    Navbar design1

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -7,25 +7,28 @@ const FullScreenNav = () => {
                 <div className='link border-t' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
                     <div className='absolute flex'>
-                        <div>
-                            <h2>Pour tout voir </h2>
+                        <div className='flex items-center'>
+                            <h2 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
+                            <h2 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
                         </div>
 
-                        <div>
+                        {/* <div>
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
+                        </div> */}
                     </div>
                 </div>
 
+
+
+                {/* 
                 <div className='link border-t' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Agence </h1>
-                    {/* <div>
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Agence </h1> */}
+                {/* <div>
                         <div>
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
@@ -40,11 +43,13 @@ const FullScreenNav = () => {
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
                         </div>
                     </div> */}
-                </div>
+                {/* </div> */}
 
-                <div className='link border-t' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Contact </h1>
-                    {/* <div>
+
+
+                {/* <div className='link border-t' > */}
+                {/* <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Contact </h1> */}
+                {/* <div>
                         <div>
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
@@ -59,11 +64,11 @@ const FullScreenNav = () => {
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
                         </div>
                     </div> */}
-                </div>
+                {/* </div> */}
 
-                <div className='link border-t' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Blogue </h1>
-                    {/* <div>
+                {/* <div className='link border-t' > */}
+                {/* <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Blogue </h1> */}
+                {/* <div>
                         <div>
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
@@ -78,7 +83,7 @@ const FullScreenNav = () => {
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
                         </div>
                     </div> */}
-                </div>
+                {/* </div> */}
 
             </div>
         </div>
