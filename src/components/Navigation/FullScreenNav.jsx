a8cbf9c766415e4a2a7436a0e5c4b4017487d1d0
nidhi <kumarinidhi0594@gmail.com>
2025-12-09 18:22:04 +0100

    Navbar design

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -4,9 +4,9 @@ const FullScreenNav = () => {
     return (
         <div id='fullScreenNav' className='text-white h-screen w-full absolute bg-amber-800 py-40'>
             <div id='allLinks'>
-                <div className='link border-y' >
+                <div className='link border-t' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
-                    {/* <div>
+                    <div className='absolute flex'>
                         <div>
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
@@ -20,10 +20,10 @@ const FullScreenNav = () => {
                             <h2>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
                         </div>
-                    </div> */}
+                    </div>
                 </div>
 
-                <div className='link border-y' >
+                <div className='link border-t' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Agence </h1>
                     {/* <div>
                         <div>
@@ -42,7 +42,7 @@ const FullScreenNav = () => {
                     </div> */}
                 </div>
 
-                <div className='link border-y' >
+                <div className='link border-t' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Contact </h1>
                     {/* <div>
                         <div>
@@ -61,7 +61,7 @@ const FullScreenNav = () => {
                     </div> */}
                 </div>
 
-                <div className='link border-y' >
+                <div className='link border-t' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Blogue </h1>
                     {/* <div>
                         <div>
