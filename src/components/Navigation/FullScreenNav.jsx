30cdad94a5b4e769d21719096210060789a5ca3b
nidhi <kumarinidhi0594@gmail.com>
2025-12-09 17:48:36 +0100

    Full Screen nav

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 const FullScreenNav = () => {
     return (
-        <div>
-            <div>
-                <div>
+        <div id='fullScreenNav' className='h-screen w-full absolute bg-amber-800'>
+            <div id='allLinks'>
+                <div className='link'>
                     <h1> Projects</h1>
-                    <div><h2>Pour tout voir </h2>
-                        <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                    </div>
+                    <div>
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
 
-                    <div><h2>Pour tout voir </h2>
-                        <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
                     </div>
                 </div>
             </div>
