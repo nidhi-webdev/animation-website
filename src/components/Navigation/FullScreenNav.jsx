d0cc44f81fa7d91aa518981f9ac36ed92a37cd46
nidhi <kumarinidhi0594@gmail.com>
2025-12-09 18:43:36 +0100

    Navbar design2

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -7,10 +7,10 @@ const FullScreenNav = () => {
                 <div className='link border-t' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
                     <div className='absolute flex'>
-                        <div className='flex items-center'>
-                            <h2 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
+                        <div className='flex items-center overflow-auto'>
+                            <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
+                            <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
                             <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
                         </div>
 
