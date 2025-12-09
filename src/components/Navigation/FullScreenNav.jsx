639caba0499996176996231521767fe7375cda70
nidhi <kumarinidhi0594@gmail.com>
2025-12-09 18:17:47 +0100

    Navbar Menu

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const FullScreenNav = () => {
     return (
-        <div id='fullScreenNav' className='h-screen w-full absolute bg-amber-800 py-40'>
+        <div id='fullScreenNav' className='text-white h-screen w-full absolute bg-amber-800 py-40'>
             <div id='allLinks'>
-                <div className='link border-y-2' >
-                    <h1 className='font-[font1] text-[17vw] uppercase'> Projects</h1>
+                <div className='link border-y' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
                     {/* <div>
                         <div>
                             <h2>Pour tout voir </h2>
@@ -22,6 +22,64 @@ const FullScreenNav = () => {
                         </div>
                     </div> */}
                 </div>
+
+                <div className='link border-y' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Agence </h1>
+                    {/* <div>
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
+
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
+                    </div> */}
+                </div>
+
+                <div className='link border-y' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Contact </h1>
+                    {/* <div>
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
+
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
+                    </div> */}
+                </div>
+
+                <div className='link border-y' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Blogue </h1>
+                    {/* <div>
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
+
+                        <div>
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
+                            <h2>Pour tout voir </h2>
+                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
+                        </div>
+                    </div> */}
+                </div>
+
             </div>
         </div>
     )
