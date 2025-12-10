de1ea46ce6cbd123a24134e6ead631f229112c6d
nidhi <kumarinidhi0594@gmail.com>
2025-12-10 19:10:08 +0100

    Design

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -4,16 +4,17 @@ const FullScreenNav = () => {
     return (
         <div id='fullScreenNav' className='text-white h-screen w-full absolute bg-amber-800 py-40'>
             <div id='allLinks'>
-                <div className='link border-t' >
+                <div className='link border-t relative' >
                     <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
-                    <div className='w-full overflow-x-auto overflow-y-hidden'>
-                        <div className='flex items-center gap-4 w-max'>
+                    {/* <div className='absolute flex'> */}
+                    <div className='flex w-full overflow-x-auto overflow-y-hidden absolute top-0'>
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
                             <img className='h-32 w-80 object-cover rounded-full shrink-0' src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' alt="Project 1" />
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'>Pour tout voir </h2>
                             <img className='h-32 w-80 object-cover rounded-full shrink-0' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt="Project 2" />
-                            </div>
+                            
                     </div>
+                    {/* </div> */}
                 </div>
 
 
