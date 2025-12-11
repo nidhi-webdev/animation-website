df7fbf1fff6c443a51167b5886adefb8ec7df551
nidhi <kumarinidhi0594@gmail.com>
2025-12-11 17:31:43 +0100

    TEXT

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -29,8 +29,8 @@ const FullScreenNav = () => {
                     </div>
                 </div>
 
-                 <div className='link border-y relative ' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
+                <div className='link border-y relative ' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Agence </h1>
                     <div className='moveLink absolute top-0 bg-[#D3FD50] text-black w-full overflow-hidden'>
                         <div className='flex items-center gap-8'>
                             <div className='flex items-center gap-8 moveX'>
@@ -54,8 +54,8 @@ const FullScreenNav = () => {
                     </div>
                 </div>
 
-                 <div className='link border-y relative ' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
+                <div className='link border-y relative ' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Contact </h1>
                     <div className='moveLink absolute top-0 bg-[#D3FD50] text-black w-full overflow-hidden'>
                         <div className='flex items-center gap-8'>
                             <div className='flex items-center gap-8 moveX'>
@@ -79,8 +79,8 @@ const FullScreenNav = () => {
                     </div>
                 </div>
 
-                 <div className='link border-y relative ' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Projects</h1>
+                <div className='link border-y relative ' >
+                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Blogue </h1>
                     <div className='moveLink absolute top-0 bg-[#D3FD50] text-black w-full overflow-hidden'>
                         <div className='flex items-center gap-8'>
                             <div className='flex items-center gap-8 moveX'>
@@ -104,67 +104,6 @@ const FullScreenNav = () => {
                     </div>
                 </div>
 
-
-
-                {/* 
-                <div className='link border-t' >
-                    <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Agence </h1> */}
-                {/* <div>
-                        <div>
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
-
-                        <div>
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
-                    </div> */}
-                {/* </div> */}
-
-
-
-                {/* <div className='link border-t' > */}
-                {/* <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Contact </h1> */}
-                {/* <div>
-                        <div>
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
-
-                        <div>
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
-                    </div> */}
-                {/* </div> */}
-
-                {/* <div className='link border-t' > */}
-                {/* <h1 className='font-[font2] text-[8vw] text-center uppercase leading-[0.8] pt-7'> Blogue </h1> */}
-                {/* <div>
-                        <div>
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
-
-                        <div>
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99' />
-                            <h2>Pour tout voir </h2>
-                            <img src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' />
-                        </div>
-                    </div> */}
-                {/* </div> */}
 
             </div>
         </div>
