d2d549568e348510651fb202bdc7655d5709d631
nidhi <kumarinidhi0594@gmail.com>
2025-12-14 18:49:50 +0100

    background color bg-black/10 for fadding the background

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
           <div className='w-1/2 h-full transition rounded-none hover:rounded-[50px] overflow-hidden'>
             <img className='h-full w-full object-cover ' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt='Image' />
           </div>
-          <div className='absolute top-0 left-0'>
+          <div className='absolute top-0 left-0 h-full w-full bg-black/10'>
             <h2></h2>
           </div>
         </div>
