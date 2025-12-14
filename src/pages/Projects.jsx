aa75fa6d2905fad38cf6056df494be19a812188b
nidhi <kumarinidhi0594@gmail.com>
2025-12-14 18:12:30 +0100

    Projet page design

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,9 +13,16 @@ const Projects = () => {
 
       <div>
         <div className='w-full h-[600px] bg-amber-300 -mt-20 flex gap-4 mb-4'>
-          <div className='w-1/2 h-full bg-green-400'></div>
+          <div className='w-1/2 h-full bg-green-400'>
+          <img className='h-full w-full' src='https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c' alt='Image'/>
+          </div>
           <div className='w-1/2 h-full bg-blue-400'></div>
         </div>
+
+        {/* <div className='w-full h-[600px] bg-amber-300  flex gap-4 mb-4'>
+          <div className='w-1/2 h-full bg-green-400'></div>
+          <div className='w-1/2 h-full bg-blue-400'></div>
+        </div> */}
       </div>
 
 
