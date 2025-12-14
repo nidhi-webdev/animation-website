42c93629efc3528fbe35e5bccd4b4c55f65644db
nidhi <kumarinidhi0594@gmail.com>
2025-12-14 17:48:45 +0100

    Projet page Started

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-      Projects
+    <div className=''>
+     <div className='bg-red-200 pt-[45vh]'>
+      <h2 className='font-[font2] text-[10vw] uppercase'>
+        Projets
+      </h2>
+     </div>
     </div>
   )
 }
