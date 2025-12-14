905e74c72ce7762501529248e571ef4662f9f6bc
nidhi <kumarinidhi0594@gmail.com>
2025-12-14 17:57:06 +0100

    Projet page Structure

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className=''>
-     <div className='bg-red-200 pt-[45vh]'>
-      <h2 className='font-[font2] text-[10vw] uppercase'>
-        Projets
-      </h2>
-     </div>
+    <div className='p-4'>
+      <div className='bg-red-200 pt-[45vh]'>
+        <h2 className='font-[font2] text-[10vw] uppercase'>
+          Projets
+        </h2>
+      </div>
+
+
+
+      <div>
+        <div className='w-full h-[600px] bg-amber-300 -mt-20 flex gap-4 mb-4'>
+          <div className='w-1/2 h-full bg-green-400'></div>
+          <div className='w-1/2 h-full bg-blue-400'></div>
+        </div>
+      </div>
+
+
+
+
     </div>
   )
 }
