b1b3d9c599196ea5d328519f4aea09d41ecd282a
nidhi <kumarinidhi0594@gmail.com>
2025-12-14 18:24:51 +0100

    Projet page design1

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='p-4'>
-      <div className='bg-red-200 pt-[45vh]'>
+      <div className='pt-[45vh]'>
         <h2 className='font-[font2] text-[10vw] uppercase'>
           Projets
         </h2>
@@ -11,10 +11,10 @@ const Projects = () => {
 
 
 
-      <div>
-        <div className='w-full h-[600px] bg-amber-300 -mt-20 flex gap-4 mb-4'>
-          <div className='w-1/2 h-full bg-green-400'>
-          <img className='h-full w-full' src='https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c' alt='Image'/>
+      <div className='-mt-10'>
+        <div className='w-full h-[600px] flex gap-4 mb-4'>
+          <div className='w-1/2 h-full hover:rounded-4xl overflow-hidden'>
+          <img className='h-full w-full object-cover' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt='Image'/>
           </div>
           <div className='w-1/2 h-full bg-blue-400'></div>
         </div>
