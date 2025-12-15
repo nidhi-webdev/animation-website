a5035a3a6d220aee26628962d5b93ccafe65082f
nidhi <kumarinidhi0594@gmail.com>
2025-12-15 18:08:36 +0100

    Project image design

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,18 +13,16 @@ const Projects = () => {
 
       <div className='-mt-10'>
         <div className='w-full h-[600px] flex gap-4 mb-4'>
-          <div className='w-1/2 h-full transition rounded-none hover:rounded-[50px] overflow-hidden'>
+          <div className='w-1/2 h-full transition rounded-none hover:rounded-[50px] overflow-hidden relative'>
             <img className='h-full w-full object-cover ' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt='Image' />
-          </div>
-          <div className='absolute top-0 left-0 h-full w-full bg-black/10'>
-            <h2></h2>
+
+            <div className='absolute top-0 left-0 h-full w-full bg-black/10'>
+              <h2 className='uppercase text-4xl font-[font2] border-2 rounded-full'> Voir le projet </h2>
+            </div>
           </div>
         </div>
 
-        {/* <div className='w-full h-[600px] bg-amber-300  flex gap-4 mb-4'>
-          <div className='w-1/2 h-full bg-green-400'></div>
-          <div className='w-1/2 h-full bg-blue-400'></div>
-        </div> */}
+    
       </div>
 
 
