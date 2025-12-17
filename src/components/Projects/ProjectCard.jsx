d5c6c043879d23535799eeb9c211d5f3ef47f1bd
nidhi <kumarinidhi0594@gmail.com>
2025-12-17 17:24:55 +0100

    Project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = (props ) => {
 
 
     return (
-        <div className='w-full h-[600px] flex gap-4 mb-4 cursor-pointer'>
+        <div className='w-full h-[800px] flex gap-4 mb-4 cursor-pointer'>
             <div className='w-1/2 h-full transition-all rounded-none hover:rounded-[70px] overflow-hidden relative group'>
                 <img className='h-full w-full object-cover ' src={props.image1} alt='Image' />
 
