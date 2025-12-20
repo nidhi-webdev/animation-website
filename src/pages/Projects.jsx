1e59d9d8ead8114b3096472039bb95d09f09dcb5
nidhi <kumarinidhi0594@gmail.com>
2025-12-20 16:28:46 +0100

    Something is working

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -47,28 +47,29 @@ const Projects = () => {
   gsap.registerPlugin(ScrollTrigger)
 
   useGSAP(function () {
-    gsap.from('.hero', {
-      height: '40px',
-      stagger: {
-        amount: 0
-      },
-      scrollTrigger: {
-        trigger: '.lol',
-        start: 'top 80%',
-        end: 'top -50%',
-        scrub: true
-      }
+    const heroes = document.querySelectorAll('.hero')
+    
+    heroes.forEach((hero) => {
+      gsap.from(hero, {
+        height: '40px',
+        scrollTrigger: {
+          trigger: hero,
+          start: 'top 90%',
+          end: 'top -20%',
+          scrub: 1
+        }
+      })
     })
   })
 
   return (
-    <div className='lg:p-4 p-2 mb-[100vh]'>
+    <div className='lg:p-4 p-2 '>
       <div className=' pt-[45vh]'>
         <h2 className='font-[font2] lg:text-[9.5vw] text-7xl uppercase'>Projets</h2>
       </div>
       <div className='-lg:mt-20 lol'>
         {projects.map(function (elem, idx) {
-          return <div key={idx} className='hero w-full lg:h-[850px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
+          return <div key={idx} className='hero w-full lg:h-[500px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2 overflow-hidden'>
             <ProjectCard image1={elem.image1} image2={elem.image2} />
           </div>
         })}
