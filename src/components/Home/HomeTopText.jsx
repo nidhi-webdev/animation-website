af981ca17540fcfaf0a7e707424c1d3842ad4dbc
nidhi <kumarinidhi0594@gmail.com>
2025-12-21 15:26:29 +0100

    Removeing Unwanted code and file

--- src/components/Home/HomeTopText.jsx
+++ src/components/Home/HomeTopText.jsx
@@ -6,10 +6,10 @@ const HomeTopText = () => {
         <div className='font-[font2] text-white pt-5 text-center '>
             <div className='text-[9.5vw] uppercase leading-[8vw] flex items-center justify-center' > L'étincelle </div>
             <div className='text-[9.5vw] uppercase leading-[8vw] flex items-center justify-center'>
-                 qui
-                <div className='rounded-full w-[16vw] h-[7vw] overflow-hidden mx-4 flex items-center justify-center -mt-5'> 
-                    <Video /> 
-                    </div>
+                qui
+                <div className='rounded-full w-[16vw] h-[7vw] overflow-hidden mx-4 flex items-center justify-center -mt-5'>
+                    <Video />
+                </div>
                 génère </div>
             <div className='text-[9.5vw] uppercase leading-[9.5vw] flex items-center justify-center'> la créativité </div>
 
