1215b3ad3f00861d925ce8d2ef3bc70a081e6bcb
nidhi <kumarinidhi0594@gmail.com>
2025-12-21 15:27:37 +0100

    Removeing Unwanted code and file

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Navbar from './components/Navigation/Navbar'
 
 
 const App = () => {
-  
+
 
   return (
     <div className='overflow-x-hidden'>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                 <HomeTopText />
                 <HomeBottomText />
             </div>
-           
+
         </div>
     )
 }
